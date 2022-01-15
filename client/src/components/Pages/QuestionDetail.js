dbef523fc18aadfcdb49f5bf34ac524a10987cc1
Obie <obiemunozjr@gmail.com>
2022-01-15 07:49:48 -0500

    rm unused import

--- client/src/components/Pages/QuestionDetail.js
+++ client/src/components/Pages/QuestionDetail.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 import useToken from '../App/useToken';
-import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
