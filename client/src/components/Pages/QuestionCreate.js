186c422115d9a8bd1125e1f1ee7d243a948aff94
Obie <obiemunozjr@gmail.com>
2022-01-15 05:59:55 -0500

    Updated QuestionCreate

--- client/src/components/Pages/QuestionCreate.js
+++ client/src/components/Pages/QuestionCreate.js
@@ -1,9 +1,37 @@
 import React from 'react'
+import Box from '@mui/material/Box'
+import TextField from '@mui/material/TextField';
+import Button from "@mui/material/Button"
 
 function QuestionCreate() {
     return (
         <div>
             <h1>QuestionCreate</h1>
+            <Box
+                component="form"
+                noValidate
+                autoComplete="off"
+            >
+                <TextField
+                    id="new-question-title-field"
+                    label="Question Title"
+                    type="text"
+                    fullWidth
+                    required
+                /><br /><br />
+                <TextField
+                    id="outlined-textarea"
+                    label="Description"
+                    multiline
+                    fullWidth
+                    rows={6}
+                    required
+                />
+                <br /><br />
+                <Button variant="contained" color="primary" type="submit" size="large">Post Question</Button>
+                &nbsp;&nbsp;
+                <Button variant="contained" color="primary" size="large">Go back</Button>
+            </Box>
         </div>
     )
 }
