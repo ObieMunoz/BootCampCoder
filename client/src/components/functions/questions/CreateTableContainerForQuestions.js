7c8f7feae0c6ce329da13cc5a841d302f3612f7f
Obie <obiemunozjr@gmail.com>
2022-01-25 13:24:21 -0500

    update styles

--- client/src/components/functions/questions/CreateTableContainerForQuestions.js
+++ client/src/components/functions/questions/CreateTableContainerForQuestions.js
@@ -4,7 +4,7 @@ import Paper from '@mui/material/Paper';
 import { CreateTableForQuestions } from './CreateTableForQuestions';
 
 export function CreateTableContainerForQuestions(rows, openComment) {
-    return <TableContainer component={Paper} style={{ maxWidth: '90vw' }}>
+    return <TableContainer component={Paper} style={{ maxWidth: '90vw', display: 'flex', margin: '0 auto' }}>
         {CreateTableForQuestions(rows, openComment)}
     </TableContainer>;
 }
