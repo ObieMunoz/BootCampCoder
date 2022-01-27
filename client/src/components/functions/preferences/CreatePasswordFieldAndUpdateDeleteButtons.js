c13a37dec98f6115fa6d67f00fec7d3619e38953
Obie <obiemunozjr@gmail.com>
2022-01-27 02:48:13 -0500

    Refactor preferences: Password field, update, delete buttons

--- client/src/components/functions/preferences/CreatePasswordFieldAndUpdateDeleteButtons.js
+++ client/src/components/functions/preferences/CreatePasswordFieldAndUpdateDeleteButtons.js
@@ -3,15 +3,31 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 
 export function CreatePasswordFieldAndUpdateDeleteButtons(password, setPassword, handleUpdateGitHub, handleClickOpen) {
-    return <div style={{ backgroundColor: 'white', padding: '20px', width: '250px', borderRadius: '5px', margin: '0 auto' }}>
-        <TextField id="outlined-basic" label="Password" variant="outlined" value={password} onChange={(e) => setPassword(e.target.value)} required />
-        <br /><br />
-        <Button variant="contained" color="primary" onClick={handleUpdateGitHub}>
+    return RenderPasswordFieldAndUpdateDeleteButtons();
+
+    function RenderPasswordFieldAndUpdateDeleteButtons() {
+        return <div style={{ backgroundColor: 'white', padding: '20px', width: '250px', borderRadius: '5px', margin: '0 auto' }}>
+            {CreateTextFieldPassword()}
+            <br /><br />
+            {CreateButtonUpdateInformation()}
+            <br /><br />
+            {CreateButtonDeleteAccount()}
+        </div>;
+    }
+
+    function CreateTextFieldPassword() {
+        return <TextField id="outlined-basic" label="Password" type="password" variant="outlined" value={password} onChange={(e) => setPassword(e.target.value)} required />;
+    }
+
+    function CreateButtonUpdateInformation() {
+        return <Button variant="contained" color="primary" onClick={handleUpdateGitHub}>
             Update Information
-        </Button>
-        <br /><br />
-        <Button variant="contained" color="error" onClick={handleClickOpen}>
+        </Button>;
+    }
+
+    function CreateButtonDeleteAccount() {
+        return <Button variant="contained" color="error" onClick={handleClickOpen}>
             Delete Account
-        </Button>
-    </div>;
+        </Button>;
+    }
 }
