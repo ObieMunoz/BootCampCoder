e732b4ff509da043f57bb6707c1206ff08dd8baa
Obie <obiemunozjr@gmail.com>
2022-01-27 02:50:56 -0500

    Extract functions and create render method

--- client/src/components/functions/preferences/CreateTableRowAccountInformationAccountTimestamps.js
+++ client/src/components/functions/preferences/CreateTableRowAccountInformationAccountTimestamps.js
@@ -3,22 +3,34 @@ import TableRow from '@mui/material/TableRow';
 import { StyledTableCell } from '../styles/StyledTableCell';
 
 export function CreateTableRowAccountInformationAccountTimestamps(bearer) {
-    return <>
-        <TableRow>
-            <StyledTableCell align="center">
-                Account Created
-            </StyledTableCell>
-            <StyledTableCell align="center">
-                {new Date(bearer.created_at).toLocaleString()}
-            </StyledTableCell>
-        </TableRow>
-        <TableRow>
+    return RenderTableRowsWithAccountTimestamps();
+
+    function RenderTableRowsWithAccountTimestamps() {
+        return <>
+            {CreateTableRowAccountCreatedDate()}
+            {CreateTableRowAccountUpdatedDate()}
+        </>;
+    }
+
+    function CreateTableRowAccountUpdatedDate() {
+        return <TableRow>
             <StyledTableCell align="center">
                 Account Updated
             </StyledTableCell>
             <StyledTableCell align="center">
                 {new Date(bearer.updated_at).toLocaleString()}
             </StyledTableCell>
-        </TableRow>
-    </>;
+        </TableRow>;
+    }
+
+    function CreateTableRowAccountCreatedDate() {
+        return <TableRow>
+            <StyledTableCell align="center">
+                Account Created
+            </StyledTableCell>
+            <StyledTableCell align="center">
+                {new Date(bearer.created_at).toLocaleString()}
+            </StyledTableCell>
+        </TableRow>;
+    }
 }
