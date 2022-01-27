eff77e38f4a89711736fdeb380a2908d68e3855e
Obie <obiemunozjr@gmail.com>
2022-01-27 02:10:09 -0500

    Updated REGEX

--- client/src/components/functions/github/CreateTableCellAndCleanRepositoryName.js
+++ client/src/components/functions/github/CreateTableCellAndCleanRepositoryName.js
@@ -3,6 +3,6 @@ import { StyledTableCell } from "../styles/StyledTableCell";
 
 export function CreateTableCellAndCleanRepositoryName(repo) {
     return <StyledTableCell align="center">
-        {repo.name.split(/-|_/).map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')}
+        {repo.name.split(/-|_|./).map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')}
     </StyledTableCell>;
 }
