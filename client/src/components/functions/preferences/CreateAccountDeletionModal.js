bd302d2760bdb27a2d7f8554d42a9cb7ea86bc83
Obie <obiemunozjr@gmail.com>
2022-01-27 02:43:14 -0500

    Refactor acount deletion modal

--- client/src/components/functions/preferences/CreateAccountDeletionModal.js
+++ client/src/components/functions/preferences/CreateAccountDeletionModal.js
@@ -8,29 +8,53 @@ import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 
 export function CreateAccountDeletionModal(open, handleClose, deletionEMail, setDeletionEMail, handleDeleteAccount) {
-    return <Dialog open={open} onClose={handleClose}>
-        <DialogTitle>Delete Account</DialogTitle>
-        <DialogContent>
-            <DialogContentText>
-                This action is irreversible. Are you sure you want to delete your account?
+    return RenderDialog();
 
-                Type your e-mail address as confirmation.
-            </DialogContentText>
-            <TextField
-                autoFocus
-                margin="dense"
-                id="deletion-password-field"
-                label="E-Mail Address"
-                type="E-Mail Address"
-                fullWidth
-                variant="standard"
-                value={deletionEMail}
-                onChange={(e) => setDeletionEMail(e.target.value)}
-                required />
-        </DialogContent>
-        <DialogActions>
+    function RenderDialog() {
+        return <Dialog open={open} onClose={handleClose}>
+            {CreateDialogTitle()}
+            {CreateDialogContentWithTextAndTextField()}
+            {CreateDialogActionButtons()}
+        </Dialog>;
+    }
+
+    function CreateDialogActionButtons() {
+        return <DialogActions>
             <Button onClick={handleClose} value="cancel">Cancel</Button>
             <Button onClick={handleDeleteAccount} value="delete-account" color="error">Yes, Delete Account</Button>
-        </DialogActions>
-    </Dialog>;
+        </DialogActions>;
+    }
+
+    function CreateDialogContentWithTextAndTextField() {
+        return <DialogContent>
+            {CreateDialogContentText()}
+            {CreateDialogTextField()}
+        </DialogContent>;
+    }
+
+    function CreateDialogTextField() {
+        return <TextField
+            autoFocus
+            margin="dense"
+            id="deletion-password-field"
+            label="E-Mail Address"
+            type="E-Mail Address"
+            fullWidth
+            variant="standard"
+            value={deletionEMail}
+            onChange={(e) => setDeletionEMail(e.target.value)}
+            required />;
+    }
+
+    function CreateDialogTitle() {
+        return <DialogTitle>Delete Account</DialogTitle>;
+    }
+
+    function CreateDialogContentText() {
+        return <DialogContentText>
+            This action is irreversible. Are you sure you want to delete your account?
+
+            Type your e-mail address as confirmation.
+        </DialogContentText>;
+    }
 }
