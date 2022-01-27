e1fb0cace88bb835ea5b24038c3191288ba94c47
Obie <obiemunozjr@gmail.com>
2022-01-27 02:49:41 -0500

    Rendering table headers

--- client/src/components/functions/preferences/CreateTableHeadersAccountInformation.js
+++ client/src/components/functions/preferences/CreateTableHeadersAccountInformation.js
@@ -4,10 +4,18 @@ import TableRow from '@mui/material/TableRow';
 import { StyledTableCell } from '../styles/StyledTableCell';
 
 export function CreateTableHeadersAccountInformation() {
-    return <TableHead>
-        <TableRow>
+    return RenderTableHeaders();
+
+    function RenderTableHeaders() {
+        return <TableHead>
+            {CreateTableRowAccountInformation()}
+        </TableHead>;
+    }
+
+    function CreateTableRowAccountInformation() {
+        return <TableRow>
             <StyledTableCell align="center">ACCOUNT INFORMATION</StyledTableCell>
             <StyledTableCell align="center">USER PROFILE</StyledTableCell>
-        </TableRow>
-    </TableHead>;
+        </TableRow>;
+    }
 }
