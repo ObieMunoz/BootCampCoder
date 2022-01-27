ab5e37564ece06c07846bc9681cf7a975a0e5f6f
Obie <obiemunozjr@gmail.com>
2022-01-27 02:46:04 -0500

    Refactor modal render

--- client/src/components/functions/preferences/CreateButtonsAndModalForAccountInformationEditMode.js
+++ client/src/components/functions/preferences/CreateButtonsAndModalForAccountInformationEditMode.js
@@ -3,16 +3,23 @@ import { CreateAccountDeletionModal } from './CreateAccountDeletionModal';
 import { CreatePasswordFieldAndUpdateDeleteButtons } from './CreatePasswordFieldAndUpdateDeleteButtons';
 
 export function CreateButtonsAndModalForAccountInformationEditMode(editMode, password, setPassword, handleUpdateGitHub, handleClickOpen, open, handleClose, deletionEMail, setDeletionEMail, handleDeleteAccount) {
-    return <>
+    return RenderPasswordFieldButtonsAndModalIfEditMode();
 
-        {editMode
-            ? <div style={{ textAlign: "center" }}>
-                {CreatePasswordFieldAndUpdateDeleteButtons(password, setPassword, handleUpdateGitHub, handleClickOpen)}
-                {CreateAccountDeletionModal(open, handleClose, deletionEMail, setDeletionEMail, handleDeleteAccount)}
-            </div>
-            : null}
-        <br />
-    </>;
+    function RenderPasswordFieldButtonsAndModalIfEditMode() {
+        return <>
+            {editMode
+                ? CreatePasswordFieldButtonsAndModal()
+                : null}
+            <br />
+        </>;
+    }
+
+    function CreatePasswordFieldButtonsAndModal() {
+        return <div style={{ textAlign: "center" }}>
+            {CreatePasswordFieldAndUpdateDeleteButtons(password, setPassword, handleUpdateGitHub, handleClickOpen)}
+            {CreateAccountDeletionModal(open, handleClose, deletionEMail, setDeletionEMail, handleDeleteAccount)}
+        </div>;
+    }
 }
 
 
