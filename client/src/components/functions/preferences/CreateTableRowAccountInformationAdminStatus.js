978fc19d4a6fed11e16950075f455e882b96c449
Obie <obiemunozjr@gmail.com>
2022-01-27 02:52:33 -0500

    Extract functions and create render method

--- client/src/components/functions/preferences/CreateTableRowAccountInformationAdminStatus.js
+++ client/src/components/functions/preferences/CreateTableRowAccountInformationAdminStatus.js
@@ -3,12 +3,20 @@ import TableRow from '@mui/material/TableRow';
 import { StyledTableCell } from '../styles/StyledTableCell';
 
 export function CreateTableRowAccountInformationAdminStatus(bearer) {
-    return <TableRow>
-        <StyledTableCell align="center">
-            Admin
-        </StyledTableCell>
-        <StyledTableCell align="center">
-            {bearer.admin ? "Full Access" : "No Admin Privileges"}
-        </StyledTableCell>
-    </TableRow>;
+    return RenderTableRowAccountInformationAdminStatus();
+
+    function RenderTableRowAccountInformationAdminStatus() {
+        return <TableRow>
+            <StyledTableCell align="center">
+                Admin
+            </StyledTableCell>
+            <StyledTableCell align="center">
+                {DisplayTextBasedOnAdminStatus()}
+            </StyledTableCell>
+        </TableRow>;
+    }
+
+    function DisplayTextBasedOnAdminStatus() {
+        return bearer.admin ? "Full Access" : "No Admin Privileges";
+    }
 }
